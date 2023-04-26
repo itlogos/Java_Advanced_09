e55965daf12e192a7a6eeec80d9ad5c383918bae
olegnik <olegnik11@ukr.net>
2023-04-26 23:57:41 +0300

    First commit

---
+++ JavaAdvansed_09/src/main/webapp/js/serverCalls.js
@@ -0,0 +1,38 @@
+
+$("button.createMagazine")
+		.click(
+				function() {
+					
+					var name = $("form.createMagazine input.magazineName").val();
+					var description = $("form.createMagazine input.magazineDescription").val();
+					var price = $("form.createMagazine input.magazinePrice").val();
+				
+						var magazine = {
+								name : name,
+								description : description,
+								price : price
+						};
+//add validation
+						$.post("magazine", magazine,
+								function(data) {
+									if (data == 'Success') {
+//										$("form")[0].reset();
+//										$("form")[1].reset();
+//										showAlertAfterRegistration();
+										alert('Success');
+									}
+								});
+					
+				});
+$("button.buy-magazine").click(function() {
+	var magazineId = jQuery(this).attr("magazine-id");
+	
+	
+	$.post("subscribe", {'magazineId':magazineId},
+			function(data) {
+				if (data == 'Success') {
+$("[data-dismiss=modal]").trigger({ type: "click" });
+					alert('Success');
+				}
+			});
+});
